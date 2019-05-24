70362b850dc037ec5523ef1ab55ebcf78949b5b9
apziaka <apostolia.zk@gmail.com>
2019-05-24 12:58:13 +0300

    adding starting projects

---
+++ src/robots.js
@@ -0,0 +1,30 @@
+export const robots = [
+{
+	id:1,
+	name: 'Alexis Papaioannou',
+	username: 'Alex',
+	email: 'alpapaio@uth.gr'	
+},
+
+{
+	id:2,
+	name: 'Nikos Karakwstas',
+	username: 'Nick',
+	email: 'nickkarakwstas@gmail.com'
+},
+
+{
+	id:3,
+	name: 'Evaggelia Theodorou',
+	username: 'Vaggelitsa',
+	email: 'evaggelia.mail@gmail.com '
+},
+
+{
+	id:4,
+	name: 'Katerina Karagianni',
+	username: 'Katerinoulli',
+	email: 'katerinaki49@hotmail.com '
+}
+
+]
